e7affd1435d03b1046c8ed1a8f63a0333f963566
vmkul <55065764+vmkul@users.noreply.github.com>
2021-03-06 16:48:27 +0200

    Remove unneeded functions

--- src/client.js
+++ src/client.js
@@ -1,33 +1,10 @@
 import net from 'net';
-
 import { PORT } from './constants.js';
 import Agent from './protocol.js';
 import Messenger from './transport.js';
-import { Directory, File } from './dirtree.js';
-
-const dir = new Directory('source');
-
-const sendAllFilesInDir = async (dir, agent) => {
-  for (const entry of dir.contents) {
-    if (entry instanceof File) {
-      await agent.sendFile(entry);
-    }
-  }
-};
-
-const syncDir = async (root, agent) => {
-  await agent.sendDir(root);
-  await sendAllFilesInDir(root, agent);
-  for (const entry of root.contents) {
-    if (entry instanceof Directory) {
-      await syncDir(entry, agent);
-    }
-  }
-};
 
 const client = net.createConnection({ port: PORT }, async () => {
   const messenger = new Messenger(client);
-  const agent = new Agent(messenger);
+  const agent = new Agent(messenger, 'client');
   await agent.startNegotiation();
-  await syncDir(dir, agent);
 });
