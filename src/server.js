6b187530bbdfb78db3156e38658297a75028a2dd
vmkul <55065764+vmkul@users.noreply.github.com>
2021-03-06 16:49:29 +0200

    Register connections using manager

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import net from 'net';
-
 import { PORT } from './constants.js';
 import Agent from './protocol.js';
 import Messenger from './transport.js';
+import ConnectionManager from './connection_manager.js';
+import { Directory } from './dirtree.js';
+
+const conManager = new ConnectionManager(new Directory('server'));
 
 net
-  .createServer(socket => {
+  .createServer(async socket => {
     const messenger = new Messenger(socket);
-    new Agent(messenger);
+    const agent = new Agent(messenger, 'server');
+    await conManager.addConnection(agent);
   })
   .listen(PORT);
