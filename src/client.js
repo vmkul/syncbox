19cd049ba0a26d0d6b2e8a2ef876b3736a2ab3df
vmkul <55065764+vmkul@users.noreply.github.com>
2021-03-17 01:32:13 +0200

    Create watcher on sync end

--- src/client.js
+++ src/client.js
@@ -2,9 +2,13 @@ import net from 'net';
 import { PORT } from './constants.js';
 import Agent from './protocol.js';
 import Messenger from './transport.js';
+import Watcher from './watcher.js';
 
 const client = net.createConnection({ port: PORT }, async () => {
   const messenger = new Messenger(client);
   const agent = new Agent(messenger, 'client');
   await agent.startNegotiation();
+  agent.once('transaction_end', () => {
+    new Watcher('client', agent);
+  });
 });
