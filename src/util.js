c1fb283681a7d088faec30b3fe14f73e4d4145c6
vmkul <55065764+vmkul@users.noreply.github.com>
2021-03-17 01:30:39 +0200

    Create async queue class

--- src/util.js
+++ src/util.js
@@ -8,14 +8,48 @@ const sendAllFilesInDir = async (dir, agent) => {
   }
 };
 
-const syncDir = async (root, agent) => {
-  await agent.sendDir(root);
+const syncDir = async (root, agent, isRootDir = true) => {
+  if (!isRootDir) {
+    await agent.sendDir(root);
+  }
   await sendAllFilesInDir(root, agent);
   for (const entry of root.contents) {
     if (entry instanceof Directory) {
-      await syncDir(entry, agent);
+      await syncDir(entry, agent, false);
     }
   }
 };
 
-export { sendAllFilesInDir, syncDir };
+class AsyncQueue {
+  constructor(beforeExec, afterExec) {
+    this.beforeExec = beforeExec;
+    this.afterExec = afterExec;
+    this.queue = [];
+    this.executing = false;
+  }
+
+  async addTask(func) {
+    this.queue.push(func);
+    console.log('Queue length: ' + this.queue.length);
+    if (!this.executing) {
+      await this.startExecution();
+    }
+  }
+
+  async startExecution() {
+    this.executing = true;
+    if (this.beforeExec) await this.beforeExec();
+    while (this.queue.length !== 0) {
+      const f = this.queue.shift();
+      try {
+        await f();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    if (this.afterExec) await this.afterExec();
+    this.executing = false;
+  }
+}
+
+export { sendAllFilesInDir, syncDir, AsyncQueue };
