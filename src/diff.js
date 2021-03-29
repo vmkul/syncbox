c74c8e85eca539aa56fe9de3d0f7aed893cabd7c
vmkul <55065764+vmkul@users.noreply.github.com>
2021-03-29 17:43:10 +0300

    Create Diff class for sync between clients

---
+++ src/diff.js
@@ -0,0 +1,101 @@
+import { dirname } from 'path';
+import { File, Directory } from './dirtree.js';
+
+class Diff {
+  constructor() {
+    this.filesToAdd = new Set();
+    this.dirsToAdd = new Set();
+    this.filesToUnlink = new Set();
+    this.dirsToUnlink = new Set();
+  }
+
+  addFile(path) {
+    this.ensureDirNotDeleted(dirname(path));
+    this.filesToUnlink.delete(path);
+    this.filesToAdd.add(path);
+  }
+
+  addDir(path) {
+    this.ensureDirNotDeleted(path);
+    this.dirsToAdd.add(path);
+  }
+
+  addUnlink(path) {
+    if (this.checkDirIsRemoved(dirname(path))) {
+      return;
+    }
+    this.filesToAdd.delete(path);
+    this.filesToUnlink.add(path);
+  }
+
+  addUnlinkDir(path) {
+    if (this.checkDirIsRemoved(dirname(path))) {
+      return;
+    }
+    this.ensureChildNodeNotAdded(path);
+    this.dirsToAdd.delete(path);
+    this.dirsToUnlink.add(path);
+  }
+
+  ensureDirNotDeleted(path) {
+    let parentDir = path;
+    while ((parentDir = dirname(parentDir)) !== '.') {
+      this.dirsToUnlink.delete(parentDir);
+    }
+    this.dirsToUnlink.delete(path);
+  }
+
+  ensureChildNodeNotAdded(path) {
+    for (const filePath of this.filesToAdd) {
+      if (filePath.startsWith(path)) {
+        this.filesToAdd.delete(filePath);
+      }
+    }
+    for (const dirPath of this.dirsToAdd) {
+      if (dirPath.startsWith(path)) {
+        this.dirsToAdd.delete(dirPath);
+      }
+    }
+  }
+
+  checkDirIsRemoved(path) {
+    do {
+      if (this.dirsToUnlink.has(path)) {
+        return true;
+      }
+    } while ((path = dirname(path)) !== '.');
+
+    return false;
+  }
+
+  mergeWith(diff) {
+    diff.filesToAdd.forEach(file => this.addFile(file));
+    diff.filesToUnlink.forEach(file => this.addUnlink(file));
+    diff.dirsToAdd.forEach(dir => this.addDir(dir));
+    diff.dirsToUnlink.forEach(dir => this.addUnlinkDir(dir));
+  }
+
+  async applyChanges(agent) {
+    await agent.startTransaction();
+
+    for (const dir of this.dirsToAdd) {
+      await agent.sendDir(new Directory(dir));
+    }
+
+    for (const file of this.filesToAdd) {
+      await agent.sendFile(new File(file));
+    }
+
+    for (const file of this.filesToUnlink) {
+      await agent.sendUnlink(new File(file));
+    }
+
+    for (const dir of this.dirsToUnlink) {
+      await agent.sendUnlinkDir(new Directory(dir));
+    }
+
+    await agent.endTransaction();
+  }
+}
+
+export default Diff;
