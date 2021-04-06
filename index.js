79efa502ee6afe0640362ae31cd7eea1aba33195
vmkul <55065764+vmkul@users.noreply.github.com>
2021-04-06 18:22:05 +0300

    Load and execute config

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
-import './src/server.js';
-import './src/client.js';
+import { readFile, mkdir, opendir } from 'fs/promises';
+import { fork } from 'child_process';
+
+const loadConfig = async path => {
+  const raw = await readFile(path);
+  return JSON.parse(raw.toString());
+};
+
+const checkOrCreateDir = async path => {
+  try {
+    await (await opendir(path)).close();
+  } catch (e) {
+    await mkdir(path);
+  }
+};
+
+(async () => {
+  const config = await loadConfig('config.json');
+  const { client, server } = config;
+
+  if (server.start) {
+    await checkOrCreateDir(server.syncDir);
+    process.env.SERVER_PORT = server.port;
+    process.env.SERVER_SYNC_DIR = server.syncDir;
+    fork('./src/server.js');
+  }
+
+  if (client.start) {
+    await checkOrCreateDir(client.syncDir);
+    process.env.CLIENT_PORT = client.port;
+    process.env.CLIENT_REMOTE_HOST = client.remoteHost;
+    process.env.CLIENT_SYNC_DIR = client.syncDir;
+    fork('./src/client.js');
+  }
+})();
