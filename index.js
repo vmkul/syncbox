16fc8ea9afba08a7d4723535ea3bcb2cfdce44a5
vmkul <sanik133@gmail.com>
2021-04-09 16:17:28 +0300

    Handle fs and child_process errors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import {mkdir, opendir } from 'fs/promises';
+import { mkdir, opendir } from 'fs/promises';
 import { fork } from 'child_process';
-import { server, client } from './config.js'
+import { server, client } from './config.js';
+import { exitOnError } from './src/util.js';
 
 const checkOrCreateDir = async path => {
   try {
@@ -10,25 +11,45 @@ const checkOrCreateDir = async path => {
   }
 };
 
+const ac = new AbortController();
+
 (async () => {
   if (server.start) {
-    await checkOrCreateDir(server.syncDir);
+    try {
+      await checkOrCreateDir(server.syncDir);
+    } catch (e) {
+      exitOnError(`Could not create dir ${server.syncDir}!`);
+    }
     fork('./src/server.js', {
       env: {
         SERVER_PORT: server.port,
         SERVER_SYNC_DIR: server.syncDir,
       },
-    });
+      signal: ac.signal,
+    })
+      .on('error', () => exitOnError('Could not spawn server process!'))
+      .on('exit', () => exitOnError('Server exited on error!'));
   }
 
   if (client.start) {
-    await checkOrCreateDir(client.syncDir);
+    try {
+      await checkOrCreateDir(client.syncDir);
+    } catch (e) {
+      exitOnError(`Could not create dir ${client.syncDir}!`);
+    }
     fork('./src/client.js', {
       env: {
         CLIENT_PORT: client.port,
         CLIENT_REMOTE_HOST: client.remoteHost,
         CLIENT_SYNC_DIR: client.syncDir,
       },
-    });
+      signal: ac.signal,
+    })
+      .on('error', () => exitOnError('Could not spawn client process!'))
+      .on('exit', () => exitOnError('Client exited on error!'));
   }
 })();
+
+process.on('exit', () => {
+  ac.abort();
+});
