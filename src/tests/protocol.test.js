001f97dae5b71f9b572a1694a5a254192caf2d30
vmkul <55065764+vmkul@users.noreply.github.com>
2021-04-29 22:03:54 +0300

    Extend and clean protocol tests

--- src/tests/protocol.test.js
+++ src/tests/protocol.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, jest, test, beforeEach } from '@jest/globals';
 import Agent from '../protocol';
-import { EventEmitter } from 'events';
 import { sep } from 'path';
+import Messenger from '../../__mocks__/transport';
 
 jest.mock('fs/promises');
 import { mkdir } from 'fs/promises';
@@ -9,39 +9,55 @@ import { mkdir } from 'fs/promises';
 jest.mock('fs');
 import fs from 'fs';
 import {
+  FailMessage,
   GetFileMessage,
   HandshakeMessage,
   MakeDirMessage,
   SuccessMessage,
+  TransactionMessage,
 } from '../message';
 
 describe('Tests for protocol', () => {
-  const messenger = new EventEmitter();
-  const agent = new Agent(messenger, 'dir');
+  let messenger;
+  let agent;
+  const testFile = {
+    getFullPath() {
+      return 'dir/test.txt';
+    },
+    getRelativePath() {
+      return 'test.txt';
+    },
+    getSize() {
+      return 1000;
+    },
+  };
+
+  const receiveMessage = msg => messenger.emit('message', JSON.stringify(msg));
 
   beforeEach(() => {
-    messenger.sendMessage = jest.fn();
-    messenger.getFile = jest.fn();
-    messenger.sendFile = jest.fn();
-    messenger.closeConnection = jest.fn();
-    messenger.setTimeout = jest.fn();
-    messenger.clearTimeout = jest.fn();
+    messenger = new Messenger();
+    agent = new Agent(messenger, 'dir');
+    agent.handShake = true;
   });
 
-  test('Handshake fail', async () => {
-    try {
-      await agent.processMessage('Hello');
-    } catch (e) {
+  test('Handshake fail', done => {
+    agent.handShake = false;
+    const msg = new HandshakeMessage();
+    msg.text = 'Bad message';
+
+    receiveMessage(msg);
+
+    agent.on('end', () => {
       expect(agent.handShake).toBe(false);
-    }
+      done();
+    });
   });
 
   test('Handshake', async () => {
+    agent.handShake = false;
     const handshakeMessage = new HandshakeMessage();
 
-    process.nextTick(() =>
-      messenger.emit('message', JSON.stringify(handshakeMessage))
-    );
+    process.nextTick(() => receiveMessage(handshakeMessage));
     await agent.startNegotiation();
 
     expect(messenger.sendMessage).toBeCalledWith(
@@ -51,6 +67,7 @@ describe('Tests for protocol', () => {
   });
 
   test('FILE request', async () => {
+    await agent.processMessage(new TransactionMessage());
     await agent.processMessage(new GetFileMessage('filename', 1000));
 
     expect(messenger.getFile).toHaveBeenCalledWith(`dir${sep}filename`, 1000);
@@ -65,36 +82,22 @@ describe('Tests for protocol', () => {
   });
 
   test('sendFile method', async () => {
-    const file = {
-      getFullPath() {
-        return 'dir/test.txt';
-      },
-      getRelativePath() {
-        return 'test.txt';
-      },
-      getSize() {
-        return 1000;
-      },
-    };
-
     process.nextTick(() => {
-      messenger.emit('message', JSON.stringify(new SuccessMessage()));
+      receiveMessage(new SuccessMessage());
       setTimeout(() => {
-        messenger.emit('message', JSON.stringify(new SuccessMessage()));
+        receiveMessage(new SuccessMessage());
       }, 0);
     });
-    await agent.sendFile(file);
+    await agent.sendFile(testFile);
 
     expect(messenger.sendMessage).toHaveBeenCalledWith(
       JSON.stringify(new GetFileMessage('test.txt', 1000))
     );
-    expect(messenger.sendFile).toHaveBeenCalledWith(file);
+    expect(messenger.sendFile).toHaveBeenCalledWith(testFile);
   });
 
   test('sendDir method', async () => {
-    process.nextTick(() =>
-      messenger.emit('message', JSON.stringify(new SuccessMessage()))
-    );
+    process.nextTick(() => receiveMessage(new SuccessMessage()));
     await agent.sendDir({
       getRelativePath() {
         return 'test';
@@ -107,6 +110,7 @@ describe('Tests for protocol', () => {
   });
 
   test('createDir method', async () => {
+    await agent.processMessage(new TransactionMessage());
     await agent.processMessage(new MakeDirMessage('test'));
 
     expect(mkdir).toHaveBeenCalledWith(`dir${sep}test`, { recursive: true });
@@ -116,10 +120,33 @@ describe('Tests for protocol', () => {
   });
 
   test('Unknown request', done => {
-    messenger.emit('message', JSON.stringify({ type: 'unknown' }));
+    receiveMessage({ type: 'unknown' });
 
     process.nextTick(() => {
       expect(messenger.closeConnection).toHaveBeenCalled();
+      done();
+    });
+  });
+
+  test('Timeout during transaction', done => {
+    receiveMessage(new TransactionMessage());
+    receiveMessage(new GetFileMessage('test', 1000));
+    agent.on('end', done);
+  });
+
+  test('Send method exceptions', done => {
+    agent.sendFile(testFile).catch(done);
+  });
+
+  test('createDir throws when transaction not started', done => {
+    receiveMessage(new MakeDirMessage('test'));
+
+    agent.on('end', () => {
+      expect(messenger.closeConnection).toBeCalledWith(
+        JSON.stringify(
+          new FailMessage('Operation refused. Transaction not started')
+        )
+      );
       done();
     });
   });
