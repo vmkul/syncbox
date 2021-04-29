62fc49c7fe3ce59f1a8824d36c5c53900524bdf2
vmkul <55065764+vmkul@users.noreply.github.com>
2021-04-29 22:10:47 +0300

    Fallback to default parameters if env undefined

--- src/client.js
+++ src/client.js
@@ -4,9 +4,9 @@ import Messenger from './transport.js';
 import Watcher from './watcher.js';
 import { exitOnError } from './util.js';
 
-const PORT = parseInt(process.env.CLIENT_PORT);
-const SYNC_DIR = process.env.CLIENT_SYNC_DIR;
-const HOST = process.env.CLIENT_REMOTE_HOST;
+const PORT = parseInt(process.env.CLIENT_PORT) || 3000;
+const SYNC_DIR = process.env.CLIENT_SYNC_DIR || 'client';
+const HOST = process.env.CLIENT_REMOTE_HOST || 'localhost';
 
 const client = net.createConnection({ host: HOST, port: PORT }, async () => {
   const messenger = new Messenger(client);
