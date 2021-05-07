f471412ae728349c30b9a01f4d93e3cd69fd7c51
vmkul <sanik133@gmail.com>
2021-05-07 19:40:41 +0300

    Add integrative test

--- integrative_test.sh
+++ integrative_test.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
 
+rm -rf server client
+npm start &
+sleep 2
+
+for FILE_NAME in file1 file2 file3
+do
+  dd if=/dev/random of=./client/$FILE_NAME bs=1024 count=1024 > /dev/null 2>&1
+done
+
+mkdir ./client/dir1
+sleep 0.5
+
+for FILE_NAME in file1 file2 file3
+do
+  dd if=/dev/random of=./client/dir1/$FILE_NAME bs=1024 count=1024 > /dev/null 2>&1
+done
+
+mkdir ./client/dir2
+sleep 0.5
+
+for FILE_NAME in file1 file2 file3
+do
+  dd if=/dev/random of=./client/dir2/$FILE_NAME bs=1024 count=1024 > /dev/null 2>&1
+done
+
+sleep 0.5
+rm -rf ./client/dir2
+rm ./client/file1
+
+sleep 4
+
 if diff -qr client server; then
   echo "Directories equal. Test passed."
-  exit 0
+  EXIT_CODE=0
 else
   echo "Test failed."
+  EXIT_CODE=1
   exit 1
 fi
 
+pkill node
+exit $EXIT_CODE
