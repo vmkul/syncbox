9d7b3ca5b94d169ac3577f543f1fa096f8551cf5
vmkul <55065764+vmkul@users.noreply.github.com>
2021-05-26 15:59:48 +0300

    Add tests for con manager

--- src/tests/connection_manager.test.js
+++ src/tests/connection_manager.test.js
@@ -1,8 +1,16 @@
 import { describe, expect, jest, test, beforeEach } from '@jest/globals';
 import ConnectionManager from '../connection_manager';
 import Agent from '../../__mocks__/protocol';
+import { AsyncQueue } from '../util';
+import Diff from '../diff';
 
+jest.mock('../diff');
 jest.mock('../util');
+AsyncQueue.mockImplementation(() => ({
+  async addTask(task) {
+    setImmediate(task);
+  },
+}));
 
 describe('Tests for connection manager', () => {
   let conManager;
@@ -24,5 +32,40 @@ describe('Tests for connection manager', () => {
     await conManager.addConnection(agent);
 
     expect(agent.transactionChecker).toBeTruthy();
+  });
+
+  test('queueTransaction method', async () => {
+    const agent = new Agent();
+
+    setImmediate(() => agent.emit('transaction_end'));
+    await conManager.queueTransaction(agent);
+
+    expect(conManager.agentDiff.has(agent)).toBe(true);
+  });
+
+  test('Global sync', async () => {
+    const numAgents = 10;
+    const expectedMergesPerAgent = 9;
+    const mergeWith = jest.fn();
+    const patchAgent = jest.fn();
+
+    Diff.mockImplementation(() => ({
+      mergeWith,
+      patchAgent,
+      isEmpty() {
+        return false;
+      },
+    }));
+
+    for (let i = 0; i < numAgents; i++) {
+      const agent = new Agent();
+      conManager.connections.add(agent);
+      conManager.agentDiff.set(agent, new Diff());
+    }
+
+    await conManager.syncWithConnected();
+
+    expect(mergeWith).toBeCalledTimes(numAgents * expectedMergesPerAgent);
+    expect(patchAgent).toBeCalledTimes(numAgents);
   });
 });
