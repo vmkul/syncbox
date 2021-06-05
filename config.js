cc8f25fc5c3d5cfde7f5b180a8c57cc49c87104b
vmkul <55065764+vmkul@users.noreply.github.com>
2021-06-06 02:47:42 +0300

    Use absolute paths as default syncDirs

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
+import path from 'path';
+
 const server = {
   start: true,
   port: 3000,
-  syncDir: 'server',
+  syncDir: path.join(path.resolve(), 'server'),
 };
 
 const client = {
   start: true,
   remoteHost: 'localhost',
   port: 3000,
-  syncDir: 'client',
+  syncDir: path.join(path.resolve(), 'client'),
 };
 
 export { server, client };
